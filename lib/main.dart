163f3dba54a3e6089a44a7716fd3a3769e07383c
mikevdmeijden-hub <mikevdmeijden@gmail.com>
2026-07-21 16:36:06 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@
 // ---------------------------------------------------------------------------
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:latlong2/latlong.dart' hide Path;
 import 'package:url_launcher/url_launcher.dart';
 
 import 'services.dart';
