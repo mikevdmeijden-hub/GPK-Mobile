535b774251f2589cb7aefbacae4dcdcb8d10735a
mikevdmeijden-hub <mikevdmeijden@gmail.com>
2026-07-22 08:24:25 +0200

    Update patch_manifest.py

--- patch_manifest.py
+++ patch_manifest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""Voeg de INTERNET-permissie en de app-naam toe aan het door
-flutter create gegenereerde AndroidManifest.xml."""
+"""Pas het gegenereerde AndroidManifest.xml aan: internet, app-naam, en
+<queries> zodat de app Google Maps/browser mag openen."""
 import re
 from pathlib import Path
 
@@ -16,6 +16,20 @@ if "android.permission.INTERNET" not in xml:
     )
 
 xml = re.sub(r'android:label="[^"]*"', 'android:label="GPK Parkeren"', xml, count=1)
+
+if "<queries>" not in xml:
+    queries = """    <queries>
+        <intent>
+            <action android:name="android.intent.action.VIEW"/>
+            <data android:scheme="https"/>
+        </intent>
+        <intent>
+            <action android:name="android.intent.action.VIEW"/>
+            <data android:scheme="geo"/>
+        </intent>
+    </queries>
+</manifest>"""
+    xml = xml.replace("</manifest>", queries)
 
 pad.write_text(xml, encoding="utf-8")
 print("Manifest aangepast:")
